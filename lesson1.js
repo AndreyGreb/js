8a443426f3cabf9c2ee813f3ac6e657073c9dff0
Andrey <andrey.grbeshov@gmail.com>
2021-03-03 18:45:50 +0300

    lesson1

---
+++ lesson1.js
@@ -0,0 +1,36 @@
+//1. Вывести в терминал строку “Hello World!”
+console.log("Hello world")
+
+//2. Создать переменную для каждого типа данных
+const number = 23
+const string = 'Andrey'
+const boolean = true
+const nothing = null
+let none
+const object = {}
+const symbol = Symbol()
+
+//3. Вывести в консоль редактора кода созданные переменные
+console.log(number, string, boolean, nothing, none, object, symbol)
+
+//4. Вывести ее тип через оператор typeof и функцию typeof
+console.log(typeof number)
+console.log(typeof string)
+console.log(typeof boolean)
+console.log(typeof nothing)
+console.log(typeof none)
+console.log(typeof object)
+console.log(typeof symbol)
+
+
+//5. Попробовать изменить переменную объявленную через const
+
+//6. Попробовать изменить переменную с типом object объявленную через const
+
+//7. Попробовать изменить переменную с типом object объявленную через let
+none = 21
+
+//8. Попробовать изменить переменную с типом object объявленную через var
+var obj = {}
+obj = {id:1}
+console.log(obj)
